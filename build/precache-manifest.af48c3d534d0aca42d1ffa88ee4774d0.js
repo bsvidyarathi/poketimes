516e43d5cc9421d1a4bab525119f9ee608c73a9e
bsvidyarathi <bsvidyarathi@hotmail.com>
2020-09-24 21:15:12 +1000

    React

---
+++ build/precache-manifest.af48c3d534d0aca42d1ffa88ee4774d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65664dadac1572f1aec03b0b2f3e01a8",
+    "url": "/poketimes/index.html"
+  },
+  {
+    "revision": "61f7e3b581afdb5cbc26",
+    "url": "/poketimes/static/css/main.b3888a44.chunk.css"
+  },
+  {
+    "revision": "372e40e7fbfe19d3ab52",
+    "url": "/poketimes/static/js/2.cb0ab8f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poketimes/static/js/2.cb0ab8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f7e3b581afdb5cbc26",
+    "url": "/poketimes/static/js/main.53c102d4.chunk.js"
+  },
+  {
+    "revision": "983edde110c01916bd2e",
+    "url": "/poketimes/static/js/runtime-main.e6cd60e1.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimes/static/media/pokeball.f10bf078.png"
+  }
+]);
